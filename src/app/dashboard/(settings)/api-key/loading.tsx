fca1e6ed665e145b6f9b354ec542e48b2f1631dc
Vishnu Mouli <vishnumouli0@gmail.com>
2025-04-19 12:12:28 +0530

    Added loading screens

---
+++ src/app/dashboard/(settings)/api-key/loading.tsx
@@ -0,0 +1,11 @@
+import { LoadingSpinner } from "@/components/LoadingSpinner"
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center flex-1 w-full h-full">
+      <LoadingSpinner />
+    </div>
+  )
+}
+
+export default Loading
